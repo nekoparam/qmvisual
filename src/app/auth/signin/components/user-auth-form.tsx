5fb23a3c1d8e939439f346e6482f98d5b8cd6dc5
latyas <latyas@gmail.com>
2024-11-11 12:38:03 +0800

    update

---
+++ src/app/auth/signin/components/user-auth-form.tsx
@@ -0,0 +1,139 @@
+"use client"
+
+import { HTMLAttributes, useState } from 'react'
+import { useForm } from 'react-hook-form'
+// import { Link } from 'react-router-dom'
+import  Link  from 'next/link'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { IconBrandFacebook, IconBrandGithub, IconBrandGoogle } from '@tabler/icons-react'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Button } from './button'
+import { PasswordInput } from './password-input'
+import { cn } from '@/lib/utils'
+
+interface UserAuthFormProps extends HTMLAttributes<HTMLDivElement> {}
+
+const formSchema = z.object({
+  email: z
+    .string()
+    .min(1, { message: 'Please enter your email' })
+    .email({ message: 'Invalid email address' }),
+  password: z
+    .string()
+    .min(1, {
+      message: 'Please enter your password',
+    })
+    .min(7, {
+      message: 'Password must be at least 7 characters long',
+    }),
+})
+
+export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
+  const [isLoading, setIsLoading] = useState(false)
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    setIsLoading(true)
+    console.log(data)
+
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 3000)
+  }
+
+  return (
+    <div className={cn('grid gap-6', className)} {...props}>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <div className='grid gap-2'>
+            <FormField
+              control={form.control}
+              name='email'
+              render={({ field }) => (
+                <FormItem className='space-y-1'>
+                  <FormLabel>邮箱</FormLabel>
+                  <FormControl>
+                    <Input placeholder='name@example.com' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name='password'
+              render={({ field }) => (
+                <FormItem className='space-y-1'>
+                  <div className='flex items-center justify-between'>
+                    <FormLabel>密码</FormLabel>
+                    <Link
+                      href='/auth/forgot-password'
+                      className='text-sm font-medium text-muted-foreground hover:opacity-75'
+                    >
+                      忘记密码?
+                    </Link>
+                  </div>
+                  <FormControl>
+                    <PasswordInput placeholder='********' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button className='mt-2' loading={isLoading}>
+              登录
+            </Button>
+
+            <div className='relative my-2'>
+              <div className='absolute inset-0 flex items-center'>
+                <span className='w-full border-t' />
+              </div>
+              <div className='relative flex justify-center text-xs uppercase'>
+                <span className='bg-background px-2 text-muted-foreground'>
+                  第三方登录
+                </span>
+              </div>
+            </div>
+
+            <div className='flex items-center gap-2'>
+              <Button
+                variant='outline'
+                className='w-full'
+                type='button'
+                loading={isLoading}
+                leftSection={<IconBrandGithub className='h-4 w-4' />}
+              >
+                GitHub
+              </Button>
+              <Button
+                variant='outline'
+                className='w-full'
+                type='button'
+                loading={isLoading}
+                leftSection={<IconBrandGoogle className='h-4 w-4' />}
+              >
+                Google
+              </Button>
+            </div>
+          </div>
+        </form>
+      </Form>
+    </div>
+  )
+}
