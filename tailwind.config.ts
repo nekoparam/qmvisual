38f209f67c054ecdb9a31e4f218ef7d3808b3ae5
latyas <latyas@gmail.com>
2024-11-26 03:16:52 +0800

    add sidebar dashboard

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,97 +5,115 @@ module.exports = {
   darkMode: ["class"],
   content: ["src/**/*.{ts,tsx}"],
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
-    extend: {
-      fontSize: {
-        'xs': 'calc(0.75 * var(--font-scale))',
-        'sm': 'calc(0.875 * var(--font-scale))',
-        'base': 'calc(1 * var(--font-scale))',
-        'lg': 'calc(1.125 * var(--font-scale))',
-        'xl': 'calc(1.25 * var(--font-scale))',
-        '2xl': 'calc(1.5 * var(--font-scale))',
-        '3xl': 'calc(1.875 * var(--font-scale))',
-        '4xl': 'calc(2.25 * var(--font-scale))',
-        '5xl': 'calc(3 * var(--font-scale))',
-        '6xl': 'calc(3.75 * var(--font-scale))',
-        '7xl': 'calc(4.5 * var(--font-scale))',
-        '8xl': 'calc(6 * var(--font-scale))',
-        '9xl': 'calc(8 * var(--font-scale))',
-      },
-      colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-        text: {
-          DEFAULT: "hsl(var(--text))",
-          dark: "hsl(var(--foreground))",
-        }
-      },
-      borderRadius: {
-        lg: `var(--radius)`,
-        md: `calc(var(--radius) - 2px)`,
-        sm: "calc(var(--radius) - 4px)",
-      },
-      fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
-    },
-    variants: {
-      extend: {
-        backgroundColor: ['dark'],
-        textColor: ['dark'],
-      },
-    },
+  	container: {
+  		center: 'true',
+  		padding: '2rem',
+  		screens: {
+  			'2xl': '1400px'
+  		}
+  	},
+  	extend: {
+  		fontSize: {
+  			xs: 'calc(0.75 * var(--font-scale))',
+  			sm: 'calc(0.875 * var(--font-scale))',
+  			base: 'calc(1 * var(--font-scale))',
+  			lg: 'calc(1.125 * var(--font-scale))',
+  			xl: 'calc(1.25 * var(--font-scale))',
+  			'2xl': 'calc(1.5 * var(--font-scale))',
+  			'3xl': 'calc(1.875 * var(--font-scale))',
+  			'4xl': 'calc(2.25 * var(--font-scale))',
+  			'5xl': 'calc(3 * var(--font-scale))',
+  			'6xl': 'calc(3.75 * var(--font-scale))',
+  			'7xl': 'calc(4.5 * var(--font-scale))',
+  			'8xl': 'calc(6 * var(--font-scale))',
+  			'9xl': 'calc(8 * var(--font-scale))'
+  		},
+  		colors: {
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			text: {
+  				DEFAULT: 'hsl(var(--text))',
+  				dark: 'hsl(var(--foreground))'
+  			},
+  			sidebar: {
+  				DEFAULT: 'hsl(var(--sidebar-background))',
+  				foreground: 'hsl(var(--sidebar-foreground))',
+  				primary: 'hsl(var(--sidebar-primary))',
+  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+  				accent: 'hsl(var(--sidebar-accent))',
+  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+  				border: 'hsl(var(--sidebar-border))',
+  				ring: 'hsl(var(--sidebar-ring))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: '`var(--radius)`',
+  			md: '`calc(var(--radius) - 2px)`',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		fontFamily: {
+  			sans: ["var(--font-sans)", ...fontFamily.sans]
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
+  		}
+  	},
+  	variants: {
+  		extend: {
+  			backgroundColor: ['dark'],
+  			textColor: ['dark']
+  		}
+  	}
   },
   plugins: [require("tailwindcss-animate")],
 }
